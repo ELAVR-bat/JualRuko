2a137ae5d43f021a8bdd2ecb0bc7448e5cbb9f6b
El Avl <242993@smklabor.sch.id>
2025-08-21 09:16:02 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 export const metadata = {
   title: "PS. Anita - Properti Terbaik di Pekanbaru",
   description: "Menjual Ruko, Tanah, dan Homestay di Pekanbaru",
@@ -10,6 +12,10 @@ export default function RootLayout({ children }) {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body>{children}</body>
